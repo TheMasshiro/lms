833e6777af3ca6d44a71a4903c4ce8c21c316c34
John Christian Vicente <johnc.vicente1@gmail.com>
2025-05-21 14:37:54 +0800

    feat(learner): create about page with team section and footer

---
+++ src/pages/learner/public/about.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../../components/learner/Footer";
+import TeamSection from "../../../components/learner/teamSection";
+
+const About = () => {
+  return (
+    <div className="flex flex-col items-center space-y-7 text-center">
+      <TeamSection />
+      <Footer />
+    </div>
+  )
+};
+
+export default About;
