74b0bc922cf5fa037b5446ba12503c78e8ecc792
John Christian Vicente <johnc.vicente1@gmail.com>
2025-05-21 14:30:53 +0800

    feat(user): Add routes for course enrollment and progress

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,21 +1,38 @@
 import express from "express";
 import {
+  addCourseCount,
+  addEnrollmentCount,
   addUserRating,
+  batchEnrollmentCount,
+  batchEnrollToCourse,
+  enrollToCourse,
   getUserCourseProgress,
   getUserData,
-  purchaseCourse,
+  purchaseStatus,
+  updateRoleToStudent,
   updateUserCourseProgress,
-  userEnrolledCourses,
+  userEnrolledCourses
 } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 // Get user Data
 userRouter.get("/data", getUserData);
-userRouter.post("/purchase", purchaseCourse);
+
+userRouter.get("/update-role", updateRoleToStudent);
+userRouter.post("/purchase", purchaseStatus);
 userRouter.get("/enrolled-courses", userEnrolledCourses);
+
 userRouter.post("/update-course-progress", updateUserCourseProgress);
 userRouter.post("/get-course-progress", getUserCourseProgress);
+
 userRouter.post("/add-rating", addUserRating);
+userRouter.post("/enroll-course", enrollToCourse);
+
+userRouter.post("/add-course-count", addCourseCount);
+userRouter.post("/add-enrollment-count", addEnrollmentCount);
+
+userRouter.post("/batch-enroll-course", batchEnrollToCourse);
+userRouter.post("/batch-enrollment-count", batchEnrollmentCount);
 
 export default userRouter;
