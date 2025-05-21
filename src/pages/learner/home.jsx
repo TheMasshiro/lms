596f607129d7ec61b479f6022333afd04d4cf7c1
John Christian Vicente <johnc.vicente1@gmail.com>
2025-05-21 14:39:24 +0800

    feat(learner): implement role-based home page redirection

--- src/pages/learner/home.jsx
+++ src/pages/learner/home.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import { useContext } from "react";
+import CallToAction from "../../components/learner/CallToAction";
 import Footer from "../../components/learner/Footer";
 import Hero from "../../components/learner/hero";
-import CourseSection from "../../components/learner/courseSection";
-import CallToAction from "../../components/learner/CallToAction";
+import InfoSection from "../../components/learner/InfoSection";
+import { AppContext } from "../../context/AppContext";
+import { useUser } from "@clerk/clerk-react";
+import StudentHome from "./home/student";
+import EducatorHome from "../instructor/home/educator";
 
 const Home = () => {
+  const { isStudent, isEducator } = useContext(AppContext);
+  const { user } = useUser();
+
+  if (user) {
+    if (isEducator) {
+      return (
+        <EducatorHome />
+      );
+    } else if (isStudent) {
+      return (
+        <StudentHome />
+      );
+    }
+  }
+
   return (
     <div className="flex flex-col items-center space-y-7 text-center">
       <Hero />
-      <CourseSection />
+      <InfoSection />
       <CallToAction />
       <Footer />
     </div>
