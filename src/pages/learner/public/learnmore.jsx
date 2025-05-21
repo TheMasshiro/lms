d14cf804653e3ab0fcfd246f91b2830c2526f42b
John Christian Vicente <johnc.vicente1@gmail.com>
2025-05-21 14:38:02 +0800

    feat(learner): create LearnMore page with Privacy and Footer

---
+++ src/pages/learner/public/learnmore.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../../components/learner/Footer";
+import Privacy from "./privacy";
+
+const LearnMore = () => {
+  return (
+    <div className="flex flex-col items-center space-y-7 text-center">
+      <Privacy />
+      <Footer />
+    </div>
+  );
+};
+
+export default LearnMore;
