1a4812b7aeec42a376712c8b1b2163bc333c56e7
John Christian Vicente <johnc.vicente1@gmail.com>
2025-05-21 14:36:29 +0800

    feat(tic-tac-toe): create Cell component for game board

---
+++ src/pages/learner/games/Cell.jsx
@@ -0,0 +1,11 @@
+import "../../../components/css/tictactoe.css";
+
+const Cell = ({ value, onClick }) => {
+  return (
+    <div className="cell" onClick={onClick}>
+      {value}
+    </div>
+  );
+};
+
+export default Cell;
