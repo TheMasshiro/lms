393082f660acc0e71727b70527000e0ce434e18d
John Christian Vicente <johnc.vicente1@gmail.com>
2025-05-21 14:30:11 +0800

    refactor(course): Remove lecture URL modification logic

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -20,15 +20,6 @@ export const getCourseId = async (req, res) => {
   try {
     const courseData = await Course.findById(id).populate({ path: "educator" });
 
-    // Remove lectureUrl if isPreviewFree is false
-    courseData.courseContent.forEach((chapter) => {
-      chapter.chapterContent.forEach((lecture) => {
-        if (!lecture.isPreviewFree) {
-          lecture.lectureUrl = "";
-        }
-      });
-    });
-
     res.json({ success: true, courseData });
   } catch (error) {
     res.json({ success: false, message: error.message });
