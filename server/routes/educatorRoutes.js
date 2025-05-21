765770c1730c3d1aa080f4493b4df7dc70b9a534
John Christian Vicente <johnc.vicente1@gmail.com>
2025-05-21 14:30:48 +0800

    feat(educator): Add student management and course upload features

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
+import upload from "../configs/multer.js";
 import {
   addCourse,
+  addStudents,
+  deleteStudents,
   educatorDashboardData,
   getEducatorCourses,
+  getEducatorStudents,
   getEnrolledStudentsData,
-  updateRoleToEducator,
+  getStudents,
+  studentEnrolledCourses,
+  updateRoleToEducator
 } from "../controllers/educatorController.js";
-import upload from "../configs/multer.js";
 import { protectEducator } from "../middlewares/authMiddleware.js";
 
 const educatorRouter = express.Router();
@@ -14,16 +19,27 @@ const educatorRouter = express.Router();
 // Add Educator Role
 educatorRouter.get("/update-role", updateRoleToEducator);
 
+// Add Student
+educatorRouter.post("/add-students", protectEducator, addStudents);
+
+// Remove student
+educatorRouter.post("/remove-students", protectEducator, deleteStudents);
+
 // Add Courses
 educatorRouter.post(
   "/add-course",
-  upload.single("image"),
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "file" },
+    { name: "activity" },
+  ]),
   protectEducator,
   addCourse
 );
 
 // Get Educator Courses
 educatorRouter.get("/courses", protectEducator, getEducatorCourses);
+educatorRouter.post("/student-courses", protectEducator, studentEnrolledCourses);
 
 // Get Educator Dashboard Data
 educatorRouter.get("/dashboard", protectEducator, educatorDashboardData);
@@ -34,5 +50,11 @@ educatorRouter.get(
   protectEducator,
   getEnrolledStudentsData
 );
+
+// Get all students
+educatorRouter.get("/students", protectEducator, getStudents);
+
+// Get Educator Students Data
+educatorRouter.get("/my-students/", protectEducator, getEducatorStudents);
 
 export default educatorRouter;
