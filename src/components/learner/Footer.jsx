7b0d1e0abe6b5f170d382bf22ef9096879b7015d
John Christian Vicente <johnc.vicente1@gmail.com>
2025-05-21 14:32:17 +0800

    refactor(footer): replace FontAwesome with React Icons

--- src/components/learner/Footer.jsx
+++ src/components/learner/Footer.jsx
@@ -1,11 +1,6 @@
-import React, { useState } from "react";
+import { FaMapMarkerAlt, FaPhone, FaEnvelope, FaCheck } from "react-icons/fa";
+import { useState } from "react";
 import { assets } from "../../assets/assets";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faMapMarkerAlt,
-  faPhoneAlt,
-  faEnvelope,
-} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   const [clicked, setClicked] = useState(false);
@@ -32,26 +27,17 @@ const Footer = () => {
           <h2 className="font-semibold text-black mb-4">Get in touch</h2>
           <ul className="text-sm text-gray-600 space-y-3">
             <li className="flex items-center space-x-3">
-              <FontAwesomeIcon
-                icon={faMapMarkerAlt}
-                className="text-black text-sm leading-[1.5rem]"
-              />
+              <FaMapMarkerAlt className="text-black text-sm leading-[1.5rem]" />
               <span>01 Upper Kalaklan, Olongapo City</span>
             </li>
             <li className="flex items-center space-x-2">
-              <FontAwesomeIcon
-                icon={faPhoneAlt}
-                className="text-black text-sm leading-[1.5rem]"
-              />
+              <FaPhone className="text-black text-sm leading-[1.5rem]" />
               <a href="tel:+639955068344" className="hover:text-black">
                 +639955068344
               </a>
             </li>
             <li className="flex items-center space-x-3">
-              <FontAwesomeIcon
-                icon={faEnvelope}
-                className="text-black text-sm leading-[1.5rem]"
-              />
+              <FaEnvelope className="text-black text-sm leading-[1.5rem]" />
               <a
                 href="mailto:learnify2025@gmail.com"
                 className="hover:text-black"
@@ -79,7 +65,7 @@ const Footer = () => {
               onClick={handleClick}
               className="bg-blue-600 text-white px-4 py-2 rounded-md transform transition-all duration-300 ease-in-out hover:bg-blue-700 hover:scale-105 hover:shadow-lg"
             >
-              {clicked ? "✓" : "Send"}
+              {clicked ? <FaCheck /> : "Send"}
             </button>
           </div>
         </div>
